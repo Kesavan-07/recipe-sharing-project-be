14b5877c4ac975d3192f1e89a5b0327c488bc9f0
Kesavan-07 <m.kesavaharish@gmail.com>
2025-01-30 11:55:09 +0530

    n

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,10 +9,12 @@ router.post("/login", authController.login);
 router.post("/logout", authController.logout);
 router.get("/me", auth.verifyLogin, (req, res) => {
   try {
+    if (!req.user) {
+      return res.status(404).json({ message: "User not found" });
+    }
     const { password, ...userData } = req.user.toObject(); // Exclude password
-    res.status(200).json({ user: userData });
+    res.status(200).json(userData);
   } catch (error) {
-    console.error("Error fetching current user:", error);
     res.status(500).json({ message: "Server error", error });
   }
 });
